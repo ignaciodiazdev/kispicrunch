ffb35938bf1dc5501d9ef7631d494f57ab220040
ignaciodz <ignaciodiazbr@gmail.com>
2023-11-15 18:45:04 -0500

    first commit

---
+++ data/jugadores.js
@@ -0,0 +1,99 @@
+const jugadores = [
+  {
+    ranking: 1,
+    username: "Llan",
+    horas: null,
+    ganados: null,
+    partidas: null,
+    avatar: "images/avatar/llan.png"
+  },
+  {
+    ranking: 2,
+    username: "Jeffrey",
+    horas: 1631,
+    ganados: 55,
+    partidas: 1930,
+    avatar: "images/avatar/jeffrey.png"
+  },
+  {
+    ranking: 3,
+    username: "Rayzor",
+    horas: 1923,
+    ganados: 59,
+    partidas: 2104,
+    avatar: "images/avatar/rayzor.jpg"
+  },
+  {
+    ranking: 4,
+    username: "Randee",
+    horas: 1483,
+    ganados: 50,
+    partidas: 1664,
+    avatar: "images/avatar/shaggy.png"
+  },
+  {
+    ranking: 5,
+    username: "Ches",
+    horas: 1467,
+    ganados: 56,
+    partidas: 1808,
+    avatar: "images/avatar/ches.png"
+  },
+  {
+    ranking: 6,
+    username: "Keter",
+    horas: 1293,
+    ganados: 53,
+    partidas: 1431,
+    avatar: "images/avatar/keter.png"
+  },
+  {
+    ranking: 7,
+    username: "Gatto",
+    horas: 2269,
+    ganados: 56,
+    partidas: 2621,
+    avatar: "images/avatar/gatto.png"
+  },
+  {
+    ranking: 8,
+    username: "Miclof",
+    horas: 1954,
+    ganados: 54,
+    partidas: 2162,
+    avatar: "images/avatar/miclof.png"
+  },
+  {
+    ranking: 9,
+    username: "Potato",
+    horas: 1799,
+    ganados: 59,
+    partidas: 2579,
+    avatar: "images/avatar/potato.jpg"
+  },
+  {
+    ranking: 10,
+    username: "Mijas",
+    horas: 1198,
+    ganados: 62,
+    partidas: 1568,
+    avatar: "images/avatar/mijael.png"
+  },
+  {
+    ranking: 11,
+    username: "Robs",
+    horas: 1364,
+    ganados: 50,
+    partidas: 1694,
+    avatar: "images/avatar/robs.png"
+  },
+  {
+    ranking: 12,
+    username: "Avants",
+    horas: 356,
+    ganados: 40,
+    partidas: 386,
+    avatar: "images/avatar/avants.png"
+  }
+]
+export default jugadores;
---
+++ js/main.js
@@ -0,0 +1,51 @@
+import jugadores from '../data/jugadores.js'
+
+console.log(jugadores)
+const tabla = document.querySelector('#tabla-body');
+
+const cargarTabla = () => {
+  jugadores.forEach(jugador => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <th scope="row">
+        <span>${jugador.ranking < 10 ?"0"+jugador.ranking : jugador.ranking}</span>
+      </th>
+      <td>
+        <span class="image-item">
+          <img src="${jugador.avatar}" alt="image">
+          <span>${jugador.username}</span>
+        </span>
+      </td>
+      <td>
+        ${jugador.horas ? jugador.horas : "Privado"}
+      </td>
+      <td>
+        <span class="image-item">
+          <img src="images/trofeo.png" alt="image">
+          ${jugador.ganados ? jugador.ganados+"%" : "Privado"}
+        </span>
+      </td>
+      <td>${jugador.partidas ? jugador.partidas : "Privado"}</td>
+    `;
+    tabla.appendChild(row);
+    console.log(jugador)
+  })
+}
+document.addEventListener('DOMContentLoaded',  () => {
+  var nav = document.querySelector('nav');
+
+  nav.addEventListener('click', function (event) {
+      // Verificar si el clic ocurrió en un elemento con la clase 'nav-link'
+      if (event.target.classList.contains('nav-link')) {
+          // Eliminar la clase 'highlight' de todos los elementos de navegación
+          var navLinks = document.querySelectorAll('.nav-link');
+          navLinks.forEach(function (item) {
+              item.classList.remove('active');
+          });
+
+          // Agregar la clase 'highlight' al elemento de navegación seleccionado
+          event.target.classList.add('active');
+      }
+  });
+});
+cargarTabla();
